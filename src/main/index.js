8f4f2362ccd1c2b839bc2f9dcd92b94d7f8335bb
Sebastian Czoch <sebastian@czoch.pl>
2018-07-11 20:10:20 +0200

    Proper icon for MacOS, fix issues with closing window, open links in browser

--- src/main/index.js
+++ src/main/index.js
@@ -1,11 +1,11 @@
 'use strict'
 
-const {app, BrowserWindow, Tray, nativeImage} = require('electron')
+const {app, BrowserWindow, Tray, nativeImage, Menu, shell, window, protocol} = require('electron')
 const path = require('path')
+const assetsDirectory = path.join(__dirname, 'assets')
 
 let win
 let tray
-let trayImage = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABQAAAAUCAYAAACNiR0NAAABY0lEQVR42s3UOUsDURSG4ZlgQLJYuG8BxUIsVBSbgOACghB7G1v/gBbp7AMunYpaiI0iYuVSqH9AJWoVFBTtRLGxtPKd+I2MY2YiM40HHkJC7peTe88dw/iqCqQwhIzLhOt1ACZi6Ear3n+XFdaEWZzhCtc+DhVWgwVkEXcGphT2iGOsY81HTg30IY8ddDlDh9XZEXqQKKMOy9jFC+4UmlXnxX3J69cTRvmyvnOKG7zjVdu0ZHeZ0d78NdDQQaS1bg/9qLUPJ0igVS04wQqa7b8bJtB07GlOZ1BtaL6CBDr3dFUZjWE69Az8/x2O4RIbSAYITGqtNcsN1gcdmvQCplDlWlCpEWkroV1rCsooDnYUI+ryDfu6q1HdV/uKPfu4wCgizidOWnf6HIOYxi0esIXFEuYxg15l/Kh6bOJJTx2r2wNM6haYHjzLDvzAPebQaYSouB5B2xgPeOK/Kqa5igQN+ARltWEE8MTgLQAAAABJRU5ErkJggg=="
 
 function createWindow () {
   win = new BrowserWindow({
@@ -13,29 +13,120 @@ function createWindow () {
     height: 600,
     title: "DroidMessage",
     darkTheme: true,
-    titleBarStyle: "hidden"
+    titleBarStyle: "hidden",
   })
 
   win.loadURL('https://messages.android.com')
+  win.webContents.on('new-window', (event, url)=>{
+    event.preventDefault()
+    shell.openExternal(url)
+  });
 
-  win.on('closed', function () {
-    win = null
+  win.on('close', (event) => {
+    event.preventDefault();
+    win.hide()
   })
-}
+};
 
 function createTray() {
-  tray = new Tray(nativeImage.createFromDataURL(trayImage))
+  tray = new Tray(
+    nativeImage.createFromPath(path.join(assetsDirectory, 'iconTemplate.png'))
+  )
 
   tray.on('click', () => {
     win.isVisible() ? win.hide() : win.show()
   })
 }
 
-
+function createMenu() {
+  const template = [
+    {
+      label: 'Edit',
+      submenu: [
+        {role: 'undo'},
+        {role: 'redo'},
+        {type: 'separator'},
+        {role: 'cut'},
+        {role: 'copy'},
+        {role: 'paste'},
+        {role: 'pasteandmatchstyle'},
+        {role: 'delete'},
+        {role: 'selectall'}
+      ]
+    },
+    {
+      label: 'View',
+      submenu: [
+        {role: 'reload'},
+        {role: 'forcereload'},
+        {role: 'toggledevtools'},
+        {type: 'separator'},
+        {role: 'resetzoom'},
+        {role: 'zoomin'},
+        {role: 'zoomout'},
+        {type: 'separator'},
+        {role: 'togglefullscreen'}
+      ]
+    },
+    {
+      role: 'window',
+      submenu: [
+        {role: 'minimize'},
+        {role: 'close'},
+      ]
+    }
+  ]
+  
+  if (process.platform === 'darwin') {
+    template.unshift({
+      label: app.getName(),
+      submenu: [
+        {role: 'about'},
+        {type: 'separator'},
+        {role: 'services', submenu: []},
+        {type: 'separator'},
+        {role: 'hide'},
+        {role: 'hideothers'},
+        {role: 'unhide'},
+        {type: 'separator'},
+      {
+        label: 'Quit',
+        accelerator: 'Command+Q',
+        click: function() { console.log("dupa"); app.exit(); }
+      }
+      ]
+    })
+  
+    // Edit menu
+    template[1].submenu.push(
+      {type: 'separator'},
+      {
+        label: 'Speech',
+        submenu: [
+          {role: 'startspeaking'},
+          {role: 'stopspeaking'}
+        ]
+      }
+    )
+  
+    // Window menu
+    template[3].submenu = [
+      {role: 'close'},
+      {role: 'minimize'},
+      {role: 'zoom'},
+      {type: 'separator'},
+      {role: 'front'},
+    ]
+  }
+  
+  const menu = Menu.buildFromTemplate(template)
+  Menu.setApplicationMenu(menu)
+}
 
 app.on('ready', () => {
   createTray()
   createWindow()
+  createMenu()
 })
 
 app.on('window-all-closed', function () {
